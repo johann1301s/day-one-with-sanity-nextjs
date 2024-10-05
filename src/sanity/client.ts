9d36f04d295b9bc4e7f8ef235cfc87d478a6f64f
Johannes Skuterud <johann1301s@icloud.com>
2024-10-05 15:04:50 +0200

    wip

--- src/sanity/client.ts
+++ src/sanity/client.ts
@@ -1,7 +1,7 @@
 import { createClient } from "next-sanity";
 
 export const client = createClient({
-  projectId: "3n9xuhjh",
+  projectId: "z8gldu0c",
   dataset: "production",
   apiVersion: "2024-01-01",
   useCdn: false,
